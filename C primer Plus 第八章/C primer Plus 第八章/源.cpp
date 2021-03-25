04870eeffe593a7d76aad4347176ce7103c55d97
luo <1447599451@qq.com>
2021-03-25 21:53:03 +0800

    习题

---
+++ C primer Plus 第八章/C primer Plus 第八章/源.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+//程序清单8.1 -- echo.c -- 重复输入
+//
+//int main()
+//{
+//	char ch;
+//	while ((ch = getchar()) != '#')
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+
+
